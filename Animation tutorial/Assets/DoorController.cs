034d30b4e591a67c3172eb2a970801912d62e24f
Troy Carpenter <tc601313@ohio.edu>
2017-02-28 13:26:41 -0500

    added script for opening and closing door

---
+++ Animation tutorial/Assets/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+
+
+	Animator animator;
+
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+
+	void OnGUI () {
+
+		if (GUILayout.Button ("Open Door")) 
+		{
+			animator.SetInteger ("Door Open", 1);
+			animator.SetInteger ("Door Close", 0);
+		}
+
+		if (GUILayout.Button ("Close Door")) 
+		{
+			animator.SetInteger ("Door Open", 0);
+			animator.SetInteger ("Door Close", 1);
+		}
+	}
+}
