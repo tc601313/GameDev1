7f97aa0c1520678774c8f2127b62e515a57f078d
Troy Carpenter <tc601313@ohio.edu>
2017-03-02 12:45:19 -0500

    Added buttons for scripts

--- Animation tutorial/Assets/DoorController.cs
+++ Animation tutorial/Assets/DoorController.cs
@@ -4,23 +4,25 @@ using System.Collections;
 public class DoorController : MonoBehaviour {
 
 
-	Animator animator;
-
+	public Animator animator;
+	public Rect openDoorRectangle;
+	public Rect closeDoorRectangle;
 
 	void Start () {
 		animator = GetComponent<Animator> ();
 	}
 	
 
-	void OnGUI () {
+	void OnGUI () 
+	{
 
-		if (GUILayout.Button ("Open Door")) 
+		if (GUI.Button (openDoorRectangle, "Open Door")) 
 		{
 			animator.SetInteger ("Door Open", 1);
 			animator.SetInteger ("Door Close", 0);
 		}
 
-		if (GUILayout.Button ("Close Door")) 
+		if (GUI.Button (closeDoorRectangle, "Close Door"))
 		{
 			animator.SetInteger ("Door Open", 0);
 			animator.SetInteger ("Door Close", 1);
